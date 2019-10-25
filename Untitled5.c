36919ea62e253a4e6071dc1c1849f9a8af61630b
ROMA ASI BR PARHUSIP <55613907+1914370125@users.noreply.github.com>
2019-10-25 18:20:04 +0700

    Add files via upload

---
+++ Untitled5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    printf("\tBIODATA PRIBADI\n\n");
+    printf("NAMA : ROMA ASI BR PARHUSIP\n");
+    printf("NPM : 1914370125\n");
+    printf("KELAS : REGULER 1D\n");
+    printf("PRODI : SISTEM KOMPUTER\n");
+    printf("FAKULTAS :SAINS & TEKNOLOGI\n");
+
+
+    int a=30;
+    int b=5;
+    int c;
+    c=a+b;
+    printf("baris 1 -nilai c adalah %d\n",c);
+    c=a-b;
+    printf("baris 2 -nilai c adalah %d\n",c);
+    c=a*b;
+    printf("baris 3 -nilai c adalah %d\n",c);
+    c=a/b;
+    printf("baris 4 -nilai c adalah %d\n",c);
+    c= b%a;
+    printf("baris 5 -nilai c adalah %d\n",c);
+    c=a++;
+    printf("baris 6 -nilai c adalah %d\n",c);
+    c=a--;
+    printf("baris 7 -nilai c adalah %d\n",c);
+    return 0;
+}
